13326f2bd17a6d477481f27bbbb1254ab9a10db5
George Still <georgest32@gmail.com>
2016-03-22 17:16:20 -0700

    tues afternoon

--- js/app.js
+++ js/app.js
@@ -1,55 +1,59 @@
 //Global variables
 var faceValue;
 var wageredCards = [];
+var playerPoints = 0;
+var oppoPoints = 0;
 
 var buttonToggle = function(){
     $("#playWar").toggleClass("playWarOff");
 };
 
+//Making the deck
+var Card = function(value, suit){
+  var faceValueText = [' Two', ' Three', ' Four',' Five', ' Six', ' Seven', ' Eight', ' Nine', ' Ten', ' Jack', ' Queen', ' King', ' Ace'];
+  this.value = value;
+  this.suit = suit;
+  this.faceValue = faceValueText[value];
+  this.name = this.faceValue + ' of ' + this.suit;
+};
+
+//Shuffling the deck
+var shuffleDeck = function(a){
+  var randomIze;
+  var x;
+  for (var i = a.length; i; i -= 1){
+    randomIze = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[randomIze];
+    a[randomIze] = x;
+  }
+};
+
 //Setting up to play
 var makeDeck = function() {
 
-  //Making the deck
-    var Card = function(value, suit){
-      var faceValueText = [' Two', ' Three', ' Four',' Five', ' Six', ' Seven', ' Eight', ' Nine', ' Ten', ' Jack', ' Queen', ' King', ' Ace'];
-      this.value = value;
-      this.suit = suit;
-      this.faceValue = faceValueText[value];
-      this.name = this.faceValue + ' of ' + this.suit;
-  };
-
-    var suits = [
+  var suits = [
     'Hearts',
     'Clubs',
     'Spades',
     'Diamonds'
-    ];
+  ];
 
-    this.gameDeck = [];
-    for (var i = 0; i < 13; i++){
-      for (var j = 0; j < suits.length; j++){
-        this.gameDeck.push( new Card(i, suits[j]) );
-      }
-  }
 
-  //Shuffling the deck
-  var shuffleDeck = function(a){
-    var randomIze;
-    var x;
-    for (var i = gameDeck.length; i; i -= 1){
-      randomIze = Math.floor(Math.random() * i);
-      x = a[i - 1];
-      a[i - 1] = a[randomIze];
-      a[randomIze] = x;
+  var gameDeck = [];
+
+  for (var i = 0; i < 13; i++){
+    for (var j = 0; j < suits.length; j++){
+      gameDeck.push( new Card(i, suits[j]) );
     }
-  };
+  }
 
   shuffleDeck(gameDeck);
 
   return gameDeck;
-  };
+};
 
-makeDeck();
+var gameDeck = makeDeck();
 
 //Splitting the deck in two player decks
 var playerDeck = gameDeck.splice(0,26);
@@ -57,61 +61,86 @@ var oppoDeck = gameDeck;
 
 //Play a War
 var playWar = function() {
-  console.log('Player reveals ' + playerDeck[4].name);
-  console.log('Opponent reveals ' + oppoDeck[4].name);
+  if (playerDeck.length >= 5 && oppoDeck.length >= 5){
+    console.log('Player reveals ' + playerDeck[4].name);
+    console.log('Opponent reveals ' + oppoDeck[4].name);
 
-  $( '#stageText' ).text( 'Player shows ' + playerDeck[4].name + ' and Opponent shows ' + oppoDeck[4].name );
+    $( '#stageText' ).text( 'Player shows ' + playerDeck[4].name + ' and Opponent shows ' + oppoDeck[4].name );
 
-  if (playerDeck[4].value > oppoDeck[4].value){
-    $( '#outcomeText' ).text( 'You won the war!' );
+    if (playerDeck[4].value > oppoDeck[4].value){
+      $( '#outcomeText' ).text( 'You won the war!' );
 
-    buttonToggle();
+      $( '#playerWagerList' ).append( ', ' + wageredCards[8].name );
+      $( '#oppoWagerList' ).append( ', ' + wageredCards[9].name );
+      console.log('You won the war! Phat lewt!!');
 
-    console.log('You won the war! Phat lewt!!');
+      for (var i = 0; i < wageredCards.length; i++){
+        playerDeck.push(wageredCards[i]);
+      }
 
-    wageredCards.push(playerDeck[0], playerDeck[1], playerDeck[2], playerDeck[3], playerDeck[4]);
-    wageredCards.push(oppoDeck[0], oppoDeck[1], oppoDeck[2], oppoDeck[3], oppoDeck[4]);
-
-    for (var i = 0; i < wageredCards.length; i++){
-      playerDeck.push(wageredCards[i]);
+      buttonToggle();
+      wageredCards = [];
+      playerPoints++;
     }
 
-    oppoDeck.splice(0, 5);
-    playerDeck.splice(0, 5);
+    else if (playerDeck[4].value < oppoDeck[4].value){
+      $( '#outcomeText' ).text( 'You lost the war!' );
 
-  }
+      $( '#playerWagerList' ).append( ', ' + wageredCards[8].name );
+      $( '#oppoWagerList' ).append( ', ' + wageredCards[9].name );
+      console.log('You lost the battle! zomg');
 
-  else if (playerDeck[4].value < oppoDeck[4].value){
-    $( '#outcomeText' ).text( 'You lost the war!' );
+      for (var index = 0; index < wageredCards.length; index++){
+        oppoDeck.push(wageredCards[index]);
+      }
 
-    buttonToggle();
-    console.log('You lost the battle! zomg');
-    wageredCards.push(playerDeck[0], playerDeck[1], playerDeck[2], playerDeck[3], playerDeck[4]);
-    wageredCards.push(oppoDeck[0], oppoDeck[1], oppoDeck[2], oppoDeck[3], oppoDeck[4]);
-
-    for (var index = 0; index < wageredCards.length; index++){
-      oppoDeck.push(wageredCards[index]);
+      buttonToggle();
+      wageredCards = [];
+      oppoPoints++;
     }
 
-    oppoDeck.splice(0, 5);
-    playerDeck.splice(0, 5);
+    else {
+      console.log('DOUBLE WAR!');
+      oppoDeck.splice(0, 5);
+      playerDeck.splice(0, 5);
+    }
 
-  }
+  oppoDeck.splice(0, 5);
+  playerDeck.splice(0, 5);
+}
 
   else {
-    console.log('DOUBLE WAR!');
-    buttonToggle();
-    oppoDeck.splice(0, 5);
-    playerDeck.splice(0, 5);
-  }
+    var playerSlice = playerDeck.slice(0, 3);
+    var playLength = playerSlice.length - 1;
+    // loop through the array and append each one
+    for (var i = 0; i < playLength; i++){
+      var card = playerSlice[i];
+      $( '#playerWagerList' ).append( card.name + ', ' );
+      oppoDeck.splice(0, 1);
+      wageredCards = [];
+      buttonToggle();
 
-  console.log(playerDeck, oppoDeck);
+    }
 
-  //Win condition
-  if (playerDeck.length > 49){
-    window.alert('you win!!');
-  } else if (oppoDeck.length > 49){
-    window.alert('you lose!!');
+    var oppoSlice = oppoDeck.slice(0, 3);
+    var oppoLength = oppoSlice.length - 1;
+    // loop through the array and append each one
+    for (var i = 0; i < oppoLength; i++){
+      var card = oppoSlice[i];
+      $( '#oppoWagerList' ).append( card.name + ', ' );
+      oppoDeck.splice(0, 1);
+      wageredCards = [];
+      buttonToggle();
+    }
+
+    console.log(playerDeck, oppoDeck);
+
+    //Win condition
+    if (playerDeck.length === 52){
+      window.alert('you win!!');
+    } else if (oppoDeck.length === 52){
+      window.alert('you lose!!');
+    }
   }
 
   getNames();
@@ -127,46 +156,88 @@ var playHand = function(){
   var oppoWin;
 
   $( '#stageText' ).text( 'Player shows ' + playerDeck[0].name + ' and Opponent shows ' + oppoDeck[0].name );
-  $( '#playerCardBox' ).text( playerDeck[0].name );
-  $( '#oppoCardBox' ).text( oppoDeck[0].name );
+  // $( '#playerCardBox' ).text( playerDeck[0].name );
+  // $( '#oppoCardBox' ).text( oppoDeck[0].name );
 
   if (playerDeck[0].value > oppoDeck[0].value){
+        $('.suitImg').attr('src', '../spade.png')
     $( '#outcomeText' ).text( 'You won the battle!' );
     wageredCards.push(playerDeck[0]);
     wageredCards.push(oppoDeck[0]);
+    $( '#playerWagerList' ).text( wageredCards[0].name );
+    $( '#oppoWagerList' ).text( wageredCards[1].name );
     playerDeck.push(wageredCards[0], wageredCards[1]);
     oppoDeck.splice(0, 1);
     playerDeck.splice(0, 1);
     wageredCards = [];
+    playerPoints++;
   }
 
   else if (playerDeck[0].value < oppoDeck[0].value){
+        $('.suitImg').attr('src', '../spade.png')
     $( '#outcomeText' ).text( 'You lost the battle!' );
     wageredCards.push(playerDeck[0]);
     wageredCards.push(oppoDeck[0]);
+    $( '#playerWagerList' ).text( wageredCards[0].name );
+    $( '#oppoWagerList' ).text( wageredCards[1].name );
     oppoDeck.push(wageredCards[0], wageredCards[1]);
     oppoDeck.splice(0, 1);
     playerDeck.splice(0, 1);
     wageredCards = [];
+    oppoPoints++;
   }
 
   else {
     console.log('WAR!');
     $( '#outcomeText' ).text( 'WAR!' );
-    buttonToggle();
+    if (playerDeck.length >= 5){
+      wageredCards.push(playerDeck[0], playerDeck[1], playerDeck[2], playerDeck[3], playerDeck[4]);
+      wageredCards.push(oppoDeck[0], oppoDeck[1], oppoDeck[2], oppoDeck[3], oppoDeck[4]);
+      $( '#playerWagerList' ).text( wageredCards[0].name + ',' );
+      $( '#playerWagerList' ).append( wageredCards[1].name + ',' );
+      $( '#playerWagerList' ).append( wageredCards[2].name + ',' );
+      $( '#playerWagerList' ).append( wageredCards[3].name );
+      $( '#oppoWagerList' ).text( wageredCards[4].name + ',' );
+      $( '#oppoWagerList' ).append( wageredCards[5].name + ',' );
+      $( '#oppoWagerList' ).append( wageredCards[6].name + ',' );
+      $( '#oppoWagerList' ).append( wageredCards[7].name );
+      buttonToggle();
+    } else {
+      console.log('grlglrlgrlgr');
+      var playerSlice = playerDeck.slice(0, 3);
+      var playLength = playerSlice.length - 1;
+      // loop through the array and append each one
+      for (var i = 0; i < playLength; i++){
+        var card = playerSlice[i];
+        $( '#playerWagerList' ).append( card.name + ', ' );
+        oppoDeck.splice(0, 1);
+        buttonToggle();
+
+      }
+
+      var oppoSlice = oppoDeck.slice(0, 3);
+      var oppoLength = oppoSlice.length - 1;
+      // loop through the array and append each one
+      for (var i = 0; i < oppoLength; i++){
+        var card = oppoSlice[i];
+        $( '#oppoWagerList' ).append( card.name + ', ' );
+        oppoDeck.splice(0, 1);
+        buttonToggle();
+      }
+    }
   }
 
   console.log(playerDeck, oppoDeck);
   if (playerDeck.length === 52){
-    alert.window('you win!!');
+    window.alert('you win!!');
   } else if (oppoDeck.length === 52){
-    alert.window('you lose!!');
+    window.alert('you lose!!');
   }
 
   //Win condition
-  if (playerDeck.length > 49){
-    alert.window('you win!!');
-  } else if (oppoDeck.length === 49){
+  if (playerDeck.length === 52){
+    window.alert('you win!!');
+  } else if (oppoDeck.length === 52){
     window.alert('you lose!!');
   }
 
